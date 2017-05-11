543fc98a82eab1b940dd07b61f46c7dab8b0a23f
thuong <1451010189thuong@ou.edu.vn>
2017-05-11 16:45:11 +0700

    Demo Adding images Using JS

---
+++ Add Images JS/handleFileSelect.js
@@ -0,0 +1,27 @@
+﻿function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+  
+    // Loop through the FileList and render image files as thumbnails.
+    for (var i = 0, f; f = files[i]; i++) {
+        // Only process image files.
+        if (!f.type.match('image.*')) {
+            continue;
+        }
+        var reader = new FileReader();  //biến hiện images ra   
+        // Closure to capture the file information.
+        reader.onload = (function (theFile) {
+            return function (e) {
+                // render thumbnail.
+                var span = document.createElement('span');
+                span.innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>' ,'<i class="fa fa-times time " ></i>'].join('');             	          	
+                document.getElementById('previewImg').insertBefore(span, null); //chèn images vào span dựng sẵn có ID previewImg
+            
+            };
+        })
+        (f);
+        // Read in the image file as a data URL.     
+        	reader.readAsDataURL(f);    	       
+    }
+}
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
